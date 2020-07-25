666ec03a7eb52bbe19d7ff864318d6f127b06961
Apoorva-Chouhan <apoorvachouhan27@gmail.com>
2020-07-26 03:19:10 +0530

    adding dish-details component and updating app module and menu component

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -6,23 +6,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import 'hammerjs';
 // end of my imports
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { DishDetailsComponent } from './dish-details/dish-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishDetailsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule, 
     MatToolbarModule,           //i changed the order of imports. 
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule, 
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
