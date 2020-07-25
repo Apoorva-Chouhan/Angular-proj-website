73307e48ec1205b1587c06ccbb1e5a2caafce1e4
Apoorva-Chouhan <apoorvachouhan27@gmail.com>
2020-07-25 23:23:37 +0530

    adding menu component in application

---
+++ conFusion/src/app/shared/dish.ts
@@ -0,0 +1,10 @@
+export class dish {
+    name : string;
+    id: string;
+    image: string;
+    category : string;
+    featured: boolean;
+    label : string;
+    price : string;
+    description: string;        
+}
